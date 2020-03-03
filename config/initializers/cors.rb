94898db9f2bc90e39865e2e3e02e385fda9ae0b7
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-03-03 14:09:02 -0500

    modified origins in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://www.carlieanglemire.com/'
+    origins 'https://www.carlieanglemire.com/, https://mystifying-dubinsky-0b3b70.netlify.com/'
 
     resource '*',
       headers: :any,
